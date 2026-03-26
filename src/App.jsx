d9f10233c9c1a791ea3c0604447d181897e5894a
Asier <asier.toraya@gmail.com>
2026-03-26 18:08:17 +0100

    Actions

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const firstReportSlug = reports[0]?.slug;
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route
